6c5d37f21c79c8db3c613be2aba0e551395d1863
Przemyslaw Senderski <psenderski94@gmail.com>
2020-12-28 19:05:08 +0100

    general styles added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="container">
+      <div className="app-wrapper">
+        <h1>App</h1>
+      </div>
     </div>
   );
 }
