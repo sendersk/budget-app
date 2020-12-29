f6eeece2b5f71cef398ec6deeb5904d925532710
Przemyslaw Senderski <psenderski94@gmail.com>
2020-12-29 22:04:27 +0100

    IncomeList component created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header"
 import Balance from "./components/Balance"
 import AddTransaction from "./components/AddTransaction"
+import IncomeList from "./components/IncomeList"
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Balance />
         <AddTransaction />
+        <IncomeList />
       </div>
     </div>
   );
