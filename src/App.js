cc3c9d69f5cb3b70872b5a8125d59229c0ed579e
Przemyslaw Senderski <psenderski94@gmail.com>
2020-12-29 22:11:46 +0100

    ExpenseList component created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Balance from "./components/Balance"
 import AddTransaction from "./components/AddTransaction"
 import IncomeList from "./components/IncomeList"
+import ExpenseList from "./components/ExpenseList"
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Balance />
         <AddTransaction />
         <IncomeList />
+        <ExpenseList />
       </div>
     </div>
   );
