f949642c7b978ec6062e4abc4c74b39daf4eb8cb
Przemyslaw Senderski <psenderski94@gmail.com>
2020-12-29 21:16:03 +0100

    Header components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "./components/Header"
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <div className="app-wrapper">
-        <h1>App</h1>
+        <Header />
       </div>
     </div>
   );
