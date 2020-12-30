c5176eb5295b58f717c3642315b049338899cc95
Przemyslaw Senderski <psenderski94@gmail.com>
2020-12-30 15:48:40 +0100

    totalIncome and totalExpense functions created

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Balance = () => {
-    return (
-        <div className="balance">
-            <h2>Your Balance</h2>
-            <h3>$0.00</h3>
-            <div className="income-expense">
-                <div className="plus">
-                    <h3>Income</h3>
-                    <p>+$0.00</p>
-                </div>
-                <div className="minus">
-                    <h3>Expense</h3>
-                    <p>-$0.00</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const { incomeTransactions, expenseTransactions } = useContext(GlobalContext);
 
-export default Balance
+  const incomeAmounts = incomeTransactions.map(
+    incomeTransaction => incomeTransaction.incomeAmount
+  );
+
+  const expenseAmounts = expenseTransactions.map(
+    expenseTransaction => expenseTransaction.expenseAmount
+  );
+
+  const totalIncome = incomeAmounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const totalExpenses = expenseAmounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  return (
+    <div className="balance">
+      <h2>Your Balance</h2>
+      <h3>${(totalIncome - totalExpenses).toFixed(2)}</h3>
+      <div className="income-expense">
+        <div className="plus">
+          <h3>Income</h3>
+          <p>+${totalIncome}</p>
+        </div>
+        <div className="minus">
+          <h3>Expenses</h3>
+          <p>-${totalExpenses}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Balance;
